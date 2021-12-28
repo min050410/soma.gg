97967b42ea1b794bddd6d3579da50955ddcfbd0b
youngmin <min050410@gmail.com>
2021-12-28 20:10:33 +0900

    add: github 잔디 파싱하기

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+let $;
+
+const getJandi = async (username) => {
+    try {
+        return await axios.get(`https://ghchart.rshah.org/${username}`);
+    } catch(error){
+        console.error(error);
+    }
+}
+
+getJandi("min050410")
+.then(html => {
+    $ = cheerio.load(html.data);  
+    log($('html').html())
+})
 
 //기본 route
 router.get('/', (req, res) => {
-    res.render('main')
+    res.send($('html').html());
 })
 
 //Post form으로 받아보기 test
